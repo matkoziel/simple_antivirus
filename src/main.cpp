583c3e8f8102cdc0f94762f27eeb33c2dd38dd69
matkoziel <01159025@pw.edu.pl>
2022-04-08 00:32:17 -0700

    Working on hex to uint64

--- src/main.cpp
+++ src/main.cpp
@@ -10,91 +10,94 @@ std::string quarantineDatabase;
 
 int main(int argc, char **argv) {
 
-//    std::string hex = "f5bb0c8de146c67b44babbf4e6584cc0";
-//    std::string firstPart = hex.substr(0,16);
-//    std::string secondPart = hex.substr(16,32);
-//    std::transform(firstPart.begin(), firstPart.end(),firstPart.begin(), ::toupper);
+    std::string hex = "f5bb0c8de146c67b44babbf4e6584cc0";
+    std::string firstPart = hex.substr(0,16);
+    std::string secondPart = hex.substr(16,32);
+    std::istringstream iss(firstPart);
+    uint64_t value;
+    iss >> std::hex >> value;
+    std::cout << firstPart << "\n";
+    std::cout << value << std::endl;
+
 //    std::uint64_t first = std::stol(firstPart);
-//    std::cout << firstPart << "\n";
+
 //    std::cout << secondPart << "\n";
 //    std::cout << first << "\n";
-//    std::cout << std::stoll(secondPart) << "\n";
 
-    quarantineDir= getenv("HOME");
-    quarantineDir=quarantineDir.append("/.quarantine");
-    quarantineDatabase=quarantineDir +"/.quarantine_database.csv";
-
-    CLI::App app{"Simple antivirus"};
-
-    auto scanOpt=app.add_subcommand("scan", "Scan given path");
-    auto restoreOpt=app.add_subcommand("restore", "Restore file from quarantine");
-
-    std::string scanFileName{};
-    scanOpt -> add_option("--path",scanFileName,"Path to file/directory we want to scan")
-    ->required()
-    ->check(CLI::ExistingPath);
-
-    std::string hashDatabaseStr{};
-    auto d = scanOpt -> add_option("--d",hashDatabaseStr,"Path to hash database")
-            ->required()
-            ->check(CLI::ExistingPath);
-
-    std::string restoreFileName{};
-    restoreOpt -> add_option("--path",restoreFileName,"Path to file we want to restore");
-
-    CLI11_PARSE(app, argc, argv)
-    if(!(*scanOpt || *restoreOpt)){
-        std::cout << "Subcommand is obligatory, type --help for more information\n";
-    }
-    if(*scanOpt){
-        if(*d){
-            if (std::filesystem::exists(quarantineDir)) {
-                if(!std::filesystem::exists(quarantineDatabase)){
-                    std::ofstream file(quarantineDatabase,std::ios::out);
-                    file.close();
-                }
-                std::unordered_set<std::string> hashDatabase = readDatabaseToUnorderedSet(hashDatabaseStr);
-                std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
-                scan(scanFileName, hashDatabase, quarantineDatabaseDB);
-            }
-            else {
-                std::filesystem::create_directory(quarantineDir);
-                std::filesystem::permissions(quarantineDir, std::filesystem::perms::owner_all |
-                                                            std::filesystem::perms::group_write |
-                                                            std::filesystem::perms::group_read |
-                                                            std::filesystem::perms::others_write |
-                                                            std::filesystem::perms::others_read,
-                                             std::filesystem::perm_options::replace);
-                if (std::filesystem::is_directory(quarantineDir)) {
-                    if(!std::filesystem::exists(quarantineDatabase)){
-                        std::ofstream file(quarantineDatabase,std::ios::out);
-                        file.close();
-                    }
-                    std::cout << "Successfully created quarantine directory in :" << quarantineDir << "\n";
-                    std::unordered_set<std::string> hashDatabase = readDatabaseToUnorderedSet(hashDatabaseStr);
-                    std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
-                    scan(scanFileName, hashDatabase, quarantineDatabaseDB);
-                } else {
-                    std::cerr << "Unable to create a quarantine directory in :" << quarantineDir << "\n";
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-    }
-    if(*restoreOpt){
-        std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
-        if(!restoreFromQuarantine(restoreFileName,quarantineDatabaseDB)){
-            std::cerr << "No such file in quarantine database\n";
-            return EXIT_FAILURE;
-        }
-        else {
-            std::cout << "Successfully restored file: " << restoreFileName <<" from quarantine\n";
-        }
-    }
-    return 0;
+//    quarantineDir= getenv("HOME");
+//    quarantineDir=quarantineDir.append("/.quarantine");
+//    quarantineDatabase=quarantineDir +"/.quarantine_database.csv";
+//
+//    CLI::App app{"Simple antivirus"};
+//
+//    auto scanOpt=app.add_subcommand("scan", "Scan given path");
+//    auto restoreOpt=app.add_subcommand("restore", "Restore file from quarantine");
+//
+//    std::string scanFileName{};
+//    scanOpt -> add_option("--path",scanFileName,"Path to file/directory we want to scan")
+//    ->required()
+//    ->check(CLI::ExistingPath);
+//
+//    std::string hashDatabaseStr{};
+//    auto d = scanOpt -> add_option("--d",hashDatabaseStr,"Path to hash database")
+//            ->required()
+//            ->check(CLI::ExistingPath);
+//
+//    std::string restoreFileName{};
+//    restoreOpt -> add_option("--path",restoreFileName,"Path to file we want to restore");
+//
+//    CLI11_PARSE(app, argc, argv)
+//    if(!(*scanOpt || *restoreOpt)){
+//        std::cout << "Subcommand is obligatory, type --help for more information\n";
+//    }
+//    if(*scanOpt){
+//        if(*d){
+//            if (std::filesystem::exists(quarantineDir)) {
+//                if(!std::filesystem::exists(quarantineDatabase)){
+//                    std::ofstream file(quarantineDatabase,std::ios::out);
+//                    file.close();
+//                }
+//                std::unordered_set<std::string> hashDatabase = readDatabaseToUnorderedSet(hashDatabaseStr);
+//                std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
+//                scan(scanFileName, hashDatabase, quarantineDatabaseDB);
+//            }
+//            else {
+//                std::filesystem::create_directory(quarantineDir);
+//                std::filesystem::permissions(quarantineDir, std::filesystem::perms::owner_all |
+//                                                            std::filesystem::perms::group_write |
+//                                                            std::filesystem::perms::group_read |
+//                                                            std::filesystem::perms::others_write |
+//                                                            std::filesystem::perms::others_read,
+//                                             std::filesystem::perm_options::replace);
+//                if (std::filesystem::is_directory(quarantineDir)) {
+//                    if(!std::filesystem::exists(quarantineDatabase)){
+//                        std::ofstream file(quarantineDatabase,std::ios::out);
+//                        file.close();
+//                    }
+//                    std::cout << "Successfully created quarantine directory in :" << quarantineDir << "\n";
+//                    std::unordered_set<std::string> hashDatabase = readDatabaseToUnorderedSet(hashDatabaseStr);
+//                    std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
+//                    scan(scanFileName, hashDatabase, quarantineDatabaseDB);
+//                } else {
+//                    std::cerr << "Unable to create a quarantine directory in :" << quarantineDir << "\n";
+//                    return EXIT_FAILURE;
+//                }
+//            }
+//        }
+//    }
+//    if(*restoreOpt){
+//        std::vector<std::string> quarantineDatabaseDB = readQuarantineDatabase(quarantineDatabase);
+//        if(!restoreFromQuarantine(restoreFileName,quarantineDatabaseDB)){
+//            std::cerr << "No such file in quarantine database\n";
+//            return EXIT_FAILURE;
+//        }
+//        else {
+//            std::cout << "Successfully restored file: " << restoreFileName <<" from quarantine\n";
+//        }
+//    }
+//    return 0;
 }
 
-//TODO: Skanowanie /proc coś wywala czasami
 //TODO: Zmienić opisy przy skanowaniu na sensowne
 //TODO: Testowanie
 //TODO: Zamiana stringow w tablice uint64 i dopisanie funkcji hashujacej
